4b9b0b0a08653a1e4a239a23d9c508dd9319c258
sinnamon-roll <gmthomps@ucsc.edu>
2018-05-17 12:12:36 -0700

    The adjacency has been removed from players update.

--- Outfox/js/Player.js
+++ Outfox/js/Player.js
@@ -57,79 +57,79 @@ Player.prototype.update = function() {
             this.x = this.x + size;
 	    console.log('right pressed');
         }
-    //ADJ!!!
-    if(enemy.x == (this.x + size) || enemy.x == (this.x - size) ){
-        if (enemy.y == this.y) {
-            console.log("ADJACENT R/L");
-            this.adj = true;
-        }else
-            this.adj = false;
-    }else if (enemy.y == (this.y + size) || enemy.y == (this.y - size) ){
-        if (enemy.x == this.x) {
-            console.log("ADJACENT UP/DOWN");
-            this.adj = true;
-        }else {
-            this.adj = false;
-        }
-    }else {
-        this.adj = false;
-    }
+    // //ADJ!!!
+    // if(enemy.x == (this.x + size) || enemy.x == (this.x - size) ){
+    //     if (enemy.y == this.y) {
+    //         console.log("ADJACENT R/L");
+    //         this.adj = true;
+    //     }else
+    //         this.adj = false;
+    // }else if (enemy.y == (this.y + size) || enemy.y == (this.y - size) ){
+    //     if (enemy.x == this.x) {
+    //         console.log("ADJACENT UP/DOWN");
+    //         this.adj = true;
+    //     }else {
+    //         this.adj = false;
+    //     }
+    // }else {
+    //     this.adj = false;
+    // }
     
-    //WHEN PLAYER IS ADJACENT TO ANYTHING. . .
-    if (this.adj == true) {
-        //DISPLAY INFORMATION
-        this.range.x = this.x - size;
-        this.range.y = this.y - size;
-        this.range.visible = true;
+    // //WHEN PLAYER IS ADJACENT TO ANYTHING. . .
+    // if (this.adj == true) {
+    //     //DISPLAY INFORMATION
+    //     this.range.x = this.x - size;
+    //     this.range.y = this.y - size;
+    //     this.range.visible = true;
         
-        //Keyboard input only available when adjacent
-        if (cKey.justPressed() && this.EXH > 0) {
-            //.damage() will handle the killing of sprite if necessary~
-            enemy.RPCT += this.CHAR;
-            this.EXH -= 1;
-            //play audio
-            var char = game.add.audio('charSound');
-            char.play('',0,1,false)
-            //emit sprites
-            // collision causes particle explosion
-            // add.emitter(x, y, maxParticles)
-            var charEmitter = game.add.emitter(this.x + 32, this.y + 32, 20);
-            charEmitter.makeParticles('atlas','+_green01');        // image used for particles
-            charEmitter.setAlpha(0.5, 1);                // set particle alpha (min, max)
-            charEmitter.minParticleScale = .5;        // set min/max particle size
-            charEmitter.maxParticleScale = 1.5;
-            charEmitter.setXSpeed(-50,500);            // set min/max horizontal speed
-            charEmitter.setYSpeed(-500,500);            // set min/max vertical speed
-            charEmitter.start(true, 2000, null, 20);    // (explode, lifespan, freq, quantity)
-        }
-        if (sKey.justPressed() && this.EXH > 0) {
-            //.damage() will handle the killing of sprite if necessary~
-            enemy.CTMP += this.SAR;
-            this.EXH -= 1;
-            //play audio
-            var sar = game.add.audio('sarSound');
-            sar.play('',0,1,false)
-            //emit sprites
-            // collision causes particle explosion
-            // add.emitter(x, y, maxParticles)
-            var sarEmitter = game.add.emitter(this.x + 32, this.y + 32, 20);
-            sarEmitter.makeParticles('atlas','-_red01');        // image used for particles
-            sarEmitter.setAlpha(0.5, 1);                // set particle alpha (min, max)
-            sarEmitter.minParticleScale = .5;        // set min/max particle size
-            sarEmitter.maxParticleScale = 1.5;
-            sarEmitter.setXSpeed(-50,500);            // set min/max horizontal speed
-            sarEmitter.setYSpeed(-500,500);            // set min/max vertical speed
-            sarEmitter.start(true, 2000, null, 20);    // (explode, lifespan, freq, quantity)
-        }
-    } else {
-        this.range.visible = false;
-    }
-    if(player.EXH == 0) {
-        this.tired.x = this.x + size/2;
-        this.tired.y = this.y - size/2;
-        this.tired.visible = true;
-    }else {
-        this.tired.visible = false;
-    }
+    //     //Keyboard input only available when adjacent
+    //     if (cKey.justPressed() && this.EXH > 0) {
+    //         //.damage() will handle the killing of sprite if necessary~
+    //         enemy.RPCT += this.CHAR;
+    //         this.EXH -= 1;
+    //         //play audio
+    //         var char = game.add.audio('charSound');
+    //         char.play('',0,1,false)
+    //         //emit sprites
+    //         // collision causes particle explosion
+    //         // add.emitter(x, y, maxParticles)
+    //         var charEmitter = game.add.emitter(this.x + 32, this.y + 32, 20);
+    //         charEmitter.makeParticles('atlas','+_green01');        // image used for particles
+    //         charEmitter.setAlpha(0.5, 1);                // set particle alpha (min, max)
+    //         charEmitter.minParticleScale = .5;        // set min/max particle size
+    //         charEmitter.maxParticleScale = 1.5;
+    //         charEmitter.setXSpeed(-50,500);            // set min/max horizontal speed
+    //         charEmitter.setYSpeed(-500,500);            // set min/max vertical speed
+    //         charEmitter.start(true, 2000, null, 20);    // (explode, lifespan, freq, quantity)
+    //     }
+    //     if (sKey.justPressed() && this.EXH > 0) {
+    //         //.damage() will handle the killing of sprite if necessary~
+    //         enemy.CTMP += this.SAR;
+    //         this.EXH -= 1;
+    //         //play audio
+    //         var sar = game.add.audio('sarSound');
+    //         sar.play('',0,1,false)
+    //         //emit sprites
+    //         // collision causes particle explosion
+    //         // add.emitter(x, y, maxParticles)
+    //         var sarEmitter = game.add.emitter(this.x + 32, this.y + 32, 20);
+    //         sarEmitter.makeParticles('atlas','-_red01');        // image used for particles
+    //         sarEmitter.setAlpha(0.5, 1);                // set particle alpha (min, max)
+    //         sarEmitter.minParticleScale = .5;        // set min/max particle size
+    //         sarEmitter.maxParticleScale = 1.5;
+    //         sarEmitter.setXSpeed(-50,500);            // set min/max horizontal speed
+    //         sarEmitter.setYSpeed(-500,500);            // set min/max vertical speed
+    //         sarEmitter.start(true, 2000, null, 20);    // (explode, lifespan, freq, quantity)
+    //     }
+    // } else {
+    //     this.range.visible = false;
+    // }
+    // if(player.EXH == 0) {
+    //     this.tired.x = this.x + size/2;
+    //     this.tired.y = this.y - size/2;
+    //     this.tired.visible = true;
+    // }else {
+    //     this.tired.visible = false;
+    // }
     
 }
