f3605e53b83a64147197c650fe773fb0ce7a1106
ZeroFox <cmhunt@ucsc.edu>
2018-05-20 17:48:36 -0700

    Adding game log
    Setting up base game log orientation

--- Outfox/js/main.js
+++ Outfox/js/main.js
@@ -216,6 +216,8 @@ testState.prototype = {
         // show temp grid on top of game
         game.add.sprite(0, 0, 'tempLayout');
 
+        gameLog = game.add.text(250, 352, 'Kon Kon!', { fontSize: '12px', fill: '#fff' })
+
         instructions = game.add.text(105, 352, 'Convince other foxes to join your escape effort.\nUse the arrow keys to move.\nWhen adjacent to a fox:\nPress C to bark Charismatically!\nPress S to bark Sarcastically!\nWhen out of energy, join your Best Friend and hit B to replenish it!', { fontSize: '12px', fill: '#fff' });
         
         enemyStats = game.add.text(447, 352, 'Kon Kon!', { fontSize: '12px', fill: '#fff' })
