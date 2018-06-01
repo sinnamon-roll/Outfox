259f19a1a488fd8c809f4d8d1fcd09516463af76
ZeroFox <cmhunt@ucsc.edu>
2018-06-01 16:21:56 -0700

    MainMenu & Credits Fonts
    Modified text to be the dark brown color to match game. Made headers
    bold.

--- Outfox/js/main.js
+++ Outfox/js/main.js
@@ -225,8 +225,8 @@ MainMenu.prototype = {
             }
             
             // State change instructions and intro text -----------------------------------------------
-            menuText = game.add.text(200, 320, 'Press space to start', { font: 'Fira Sans', fontSize: '28px', fill: '#000' });
-            menuText = game.add.text(80, 370, 'Press enter to see the Foxes Responsible', { font: 'Fira Sans', fontSize: '26px', fill: '#000', wordWrapWidth: '640', wordWrap: 'false' });
+            menuText = game.add.text(200, 320, 'Press space to start', { font: 'Fira Sans', fontSize: '28px', fill: '#270201' });
+            menuText = game.add.text(80, 370, 'Press enter to see the Foxes Responsible', { font: 'Fira Sans', fontSize: '26px', fill: '#270201', wordWrapWidth: '640', wordWrap: 'false' });
             
         },
         update: function(){
