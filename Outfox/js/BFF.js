874a934f2ce430c306aa44d868f48c3bc9f95a54
ZeroFox <cmhunt@ucsc.edu>
2018-06-10 16:10:30 -0700

    Fixing BFF and PC text

--- Outfox/js/BFF.js
+++ Outfox/js/BFF.js
@@ -166,7 +166,6 @@ Bff.prototype.update = function() {
     }
     
     if(this.controlled == true){
-        add2Log('Tod\'s Turn.');
         //DISPLAY STATS
         this.cursor.visible = true;
         leftName.setText(this.NAME);
@@ -178,6 +177,10 @@ Bff.prototype.update = function() {
         'Resolve: ' + this.EXH + '\n'
         ;
         setBgColorById('main-page','#ccc');
+        if(turnBFF == false){
+            add2Log('Tod\'s turn.');
+            turnBFF = true;
+        }
         if(this.adj == true) {
             //DISPLAY FOX TARGET INFO
             enemyTarget.loadTexture('UI', 's_foxTarget');
@@ -225,10 +228,11 @@ Bff.prototype.update = function() {
     if (this.controlled == true){
         if(this.waitb == true){
             console.log("Waiting");
-            add2Log(this.NAME + ' takes a moment to compose a thought.');
+            add2Log(this.NAME + ' takes a moment to compose a thought and ends their turn.');
             this.controlled = false;
             this.acted = true;
             this.waitb = false;
+            turnBFF = false;
         }
     }
     if(this.controlled == false && this.moveable == false && this.acted == true){
