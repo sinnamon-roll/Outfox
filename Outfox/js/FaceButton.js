d2431b60b649a8221a30f3ebdd137e51b6b4aac6
ZeroFox <cmhunt@ucsc.edu>
2018-06-10 19:19:20 -0700

    Updated Face Button Dialogue
    to reflect  Immediately takes effect. in text

--- Outfox/js/FaceButton.js
+++ Outfox/js/FaceButton.js
@@ -53,7 +53,7 @@ faceButton.prototype.update = function() {
 					this.i = 0;
 					this.usable = false;
 				}else if(game.input.keyboard.justPressed(Phaser.Keyboard.SPACEBAR)){
-					add2Log('Face Action: [UP], [DOWN], [LEFT], or [RIGHT] to choose a direction to face. [SPACE] to set. May be used multiple times.', 3);
+					add2Log('Face Action: [UP], [DOWN], [LEFT], or [RIGHT] to choose a direction to face. Immediately takes effect. May be used multiple times.', 4);
 					if(player.displayed == true){
 						console.log("facing a way kinda")
 						this.faceb = 'player';
